dcdb0da116f67723a45d7f410304eb82d36bf22e
decyma <soos3121@gmail.com>
2021-12-18 07:34:50 +0000

    feat : 모델 서버 코드 수정

---
+++ model_server/service.py
@@ -0,0 +1,10 @@
+def model_init():
+  model = None
+  return model
+
+def predict_func():
+  model = model_init()
+  def predict(text):
+    predict_text = text
+    return predict_text
+  return predict
