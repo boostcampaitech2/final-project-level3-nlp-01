e39cf0f1fe94a6e2c4560ce3aa293071efd359c3
decyma <soos3121@gmail.com>
2021-12-18 07:35:45 +0000

    feat: add yaml config

--- server/app/service.py
+++ server/app/service.py
@@ -1,7 +1,17 @@
 import requests
 import json
+import yaml
 
 lang = {"한국어": "url", "english": "url", "中文": "url"}
+
+
+def load_yaml():
+    global lang
+    with open("config.yaml", "r") as f:
+        conf = yaml.load(f, Loader=yaml.FullLoader)
+    lang["한국어"] = conf["kr_url"]
+    lang["english"] = conf["en_url"]
+    lang["中文"] = conf["zh_url"]
 
 
 def translate_service(language, text):
