ddb1ce9b7f67d14ba075f427841f12c16ecf2e82
eunaoeh <eunjinhh131@gmail.com>
2021-12-20 11:33:10 +0000

    feat: 3개의 모델 서비스 추가

--- model_server/service.py
+++ model_server/service.py
@@ -1,7 +1,20 @@
 from util import model_init
 
-predict = model_init("zh")
+predict_zh = model_init("zh")
+predict_en = model_init("en")
+predict_ko = model_init("ko")
 
 
-def translate_service(text):
-    return predict(text)
+def translate_zh_service(text):
+    generate_text = predict_zh(text)
+    return generate_text
+
+
+def translate_en_service(text):
+    generate_text = predict_en(text)
+    return generate_text
+
+
+def translate_ko_service(text):
+    generate_text = predict_ko(text)
+    return generate_text
