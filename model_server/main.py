71992509faa239eba663171513b8336f6336663a
eunaoeh <eunjinhh131@gmail.com>
2021-12-20 11:32:49 +0000

    feat: 서버에 3개 모델 업로드하도록 수정

--- model_server/main.py
+++ model_server/main.py
@@ -1,15 +1,25 @@
 import uvicorn
 from fastapi import FastAPI
-from service import translate_service
+from service import translate_zh_service, translate_en_service, translate_ko_service
 from dto import ItemDto
 
 
 app = FastAPI()
 
 
-@app.post("/")
-async def translate(item: ItemDto):
-    return translate_service(item.text)
+@app.post("/zh")
+async def translate_zh(item: ItemDto):
+    return translate_zh_service(item.text)
+
+
+@app.post("/en")
+async def translate_en(item: ItemDto):
+    return translate_en_service(item.text)
+
+
+@app.post("/ko")
+async def translate_ko(item: ItemDto):
+    return translate_ko_service(item.text)
 
 
 if __name__ == "__main__":
